0ece5776067b2fbc631958009966cdac408de49d
dionpool <elirionn@gmail.com>
2022-05-10 13:08:07 +0200

    Add circles to Denmark and Netherlands

--- js/leaflet-settings.js
+++ js/leaflet-settings.js
@@ -5,32 +5,66 @@ bounds = L.latLngBounds(corner1, corner2);
 
 // Map initialisation
 var map = L.map('map', {
-wheelPxPerZoomLevel: 240,	
-zoomSnap: 0.25,
-zoomDelta: 0.5,
-minZoom: 3.25,
-maxZoom: 4.75,
-maxBounds: bounds
+    wheelPxPerZoomLevel: 240,	
+    zoomSnap: 0.25,
+    zoomDelta: 0.5,
+    minZoom: 3.25,
+    maxZoom: 4.75,
+    maxBounds: bounds
 }).setView([(56.728622, -98.4375), (-57.136239, 98.4375)], 3.25);
 
 // Reference the tiles
 L.tileLayer('images/maps/{z}/{x}/{y}.png', {
-continuousWorld: false,
-noWrap: true,
-}).addTo(map);
-
-// Marker (Leeuwarden)
-var markLeeuwarden = L.marker([-12.897489, -13.535156]).addTo(map);
-markLeeuwarden.bindPopup("<strong>Leeuwarden</strong>");
-
-// Marker (Helsingor)
-var markHelsingor = L.marker([15.707663, 24.697266]).on('click', function() {
-window.open('http://www.soundtoll.nl/index.php/nl/', '_blank');
+    continuousWorld: false,
+    noWrap: true,
 }).addTo(map);
 
 // Popup with longitude latitude
 function onMapClick(e) {
-alert(e.latlng);
+    alert(e.latlng);
 }
 
 map.on('click', onMapClick);
+
+// Cirkel Nederland
+var circleNL = L.circle([-13.939952, -12.526939], {
+    color: '#FF9B00',
+    weight: 0,
+    fillOpacity: 0,
+    radius: 700000
+}).addTo(map);
+
+circleNL.on('mouseover',function(ev) {
+    this.setStyle({
+        weight: 3,
+        fillOpacity: 0.5,
+    });
+});
+
+circleNL.on('mouseout', function(ev) {
+    this.setStyle({
+        weight: 0,
+        fillOpacity: 0,
+    });
+});
+
+// Cirkel Denemarken
+var circleDK = L.circle([15.707663, 24.697266], {
+    color: '#d1050c',
+    fillOpacity: 0.5,
+    radius: 700000
+}).addTo(map);
+
+circleDK.on('mouseover',function(ev) {
+    this.setStyle({
+        weight: 3,
+        fillOpacity: 0.5,
+    });
+});
+
+circleDK.on('mouseout', function(ev) {
+    this.setStyle({
+        weight: 0,
+        fillOpacity: 0,
+    });
+});
