05190353da7657c05bd4ae2f3a64dad09c198f3b
IwanKramer <iwanrogier@hotmail.com>
2022-05-17 14:01:07 +0200

    17/05/2022

--- js/leaflet-settings.js
+++ js/leaflet-settings.js
@@ -1,6 +1,6 @@
 // Map corners defined
-var corner1 = L.latLng(56.728622, -98.4375),
-corner2 = L.latLng(-57.136239, 98.4375),
+var corner1 = L.latLng(39.754927, -88.649383),
+corner2 = L.latLng(-48.485904, 97.605536),
 bounds = L.latLngBounds(corner1, corner2);
 
 // Map initialisation
@@ -8,10 +8,10 @@ var map = L.map('map', {
     wheelPxPerZoomLevel: 240,	
     zoomSnap: 0.25,
     zoomDelta: 0.5,
-    minZoom: 3.25,
+    minZoom: 3.75,
     maxZoom: 4.75,
     maxBounds: bounds
-}).setView([(56.728622, -98.4375), (-57.136239, 98.4375)], 3.25);
+}).setView([(39.754927, -88.649383), (-48.485904, 97.605536)], 3.75);
 
 // Reference the tiles
 L.tileLayer('images/maps/Map-Europe/{z}/{x}/{y}.png', {
