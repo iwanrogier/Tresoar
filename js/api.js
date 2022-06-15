e956073ef2d3e1073751621720ffae0bf60fa509
dionpool <elirionn@gmail.com>
2022-06-15 10:41:40 +0200

    Adds custom icon to API JavaScript

--- js/api.js
+++ js/api.js
@@ -1,13 +1,17 @@
 // Variable for cities
 var cities = null;
 
+var citiesIcon = L.icon({
+    iconUrl: '../images/pins/city.svg'
+});
+
 // Retrieve all cities from the API
 await fetch("https://sonttol-cms.herokuapp.com/api/cities")
     .then(response => response.json())
     .then(data => cities = data);
 
 cities.forEach(city => {
-    L.marker([16.105071, -28.132212])
+    L.marker([city.latitude, city.longitude], {icon: citiesIcon})
         .addTo(map)
         .bindTooltip(city.name, {permanent: true})
         .openTooltip();
